3cf0766741b784994f8586a94cb44bd74ae30dc8
biskit069 <guleycontact@gmail.com>
2025-01-08 23:47:54 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,9 +19,27 @@ def install_sslscan():
    else:  # Linux  
       subprocess.run(["sudo", "apt-get", "install", "-y", "sslscan"], check=True)  
   
+def install_metasploit():  
+   if platform.system() == "Windows":  
+      subprocess.run(["powershell", "-Command", "choco install metasploit"], check=True)  
+   elif platform.system() == "Darwin":  # macOS  
+      subprocess.run(["brew", "install", "metasploit"], check=True)  
+   else:  # Linux  
+      subprocess.run(["sudo", "apt-get", "install", "-y", "metasploit-framework"], check=True)  
+  
+def install_routersploit():  
+   if platform.system() == "Windows":  
+      subprocess.run(["powershell", "-Command", "pip install routersploit"], check=True)  
+   elif platform.system() == "Darwin":  # macOS  
+      subprocess.run(["brew", "install", "routersploit"], check=True)  
+   else:  # Linux  
+      subprocess.run(["sudo", "apt-get", "install", "-y", "routersploit"], check=True)  
+  
 def main():  
    install_nmap()  
    install_sslscan()  
+   install_metasploit()  
+   install_routersploit()  
   
 if __name__ == "__main__":  
    main()
