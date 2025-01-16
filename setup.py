eeb54f4bf7ffe162ca6f5d18b86c6f69bf1862d2
biskit <guleycontact@gmail.com>
2025-01-17 01:25:02 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,13 +53,21 @@ def install_asnmap(home_dir):
     result = subprocess.run(["go", "install", "github.com/projectdiscovery/asnmap/cmd/asnmap@latest"], text=True, capture_output=True)
     if result.returncode == 0:
         print("asnmap installed successfully.")
-        asnmap_binary = shutil.which("asnmap")
-        if asnmap_binary:
-            target_path = "/home/host/asnmap"  # Path to copy the asnmap binary
-            shutil.copy(asnmap_binary, target_path)
-            print(f"asnmap binary copied to {target_path}.")
+
+        # Get the GOPATH directory to find the binary
+        result = subprocess.run(["go", "env", "GOBIN"], text=True, capture_output=True)
+        if result.returncode == 0:
+            go_bin_dir = result.stdout.strip()
+            asnmap_binary = os.path.join(go_bin_dir, "asnmap")
+
+            if os.path.exists(asnmap_binary):
+                target_path = "/home/host/asnmap"  # Path to copy the asnmap binary
+                shutil.copy(asnmap_binary, target_path)
+                print(f"asnmap binary copied to {target_path}.")
+            else:
+                print("asnmap binary not found after installation.")
         else:
-            print("asnmap binary not found after installation.")
+            print(f"Failed to find GOPATH: {result.stderr}")
     else:
         print(f"Failed to install asnmap: {result.stderr}")
 
